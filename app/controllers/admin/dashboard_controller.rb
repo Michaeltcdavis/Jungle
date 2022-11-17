2ed32a0713e77ff843a044d9e42af93ea852a2d1
Michaeltcdavis <michaeltcdavis@gmail.com>
2022-11-17 00:45:56 +0000

    add metrics to dashboard route

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: ENV.ADMIN_AUTH_NAME, password: ENV.ADMIN_AUTH_PASSWORD
-
+  http_basic_authenticate_with name: ENV['ADMIN_AUTH_NAME'], password: ENV['ADMIN_AUTH_PASSWORD']
   def show
+    @product_count = Product.count
+    @category_count = Category.count 
   end
+
 end
