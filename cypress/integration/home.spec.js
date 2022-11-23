b483989c71ad7c723ed02fbb99b36a7c9daaf4dd
Michaeltcdavis <michaeltcdavis@gmail.com>
2022-11-23 19:57:15 +0000

    initialize cypress and write a couple test for the home page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,16 @@
+// <reference types="cypress" />
+
+describe('Home Page', () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("should show products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("should show two products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+})
